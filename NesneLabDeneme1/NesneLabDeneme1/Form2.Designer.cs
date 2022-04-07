6b614beadbc9fb5429e99feb39a56ae0776b3fb4
evenstar41 <ferdiislam4152@gmail.com>
2022-04-08 02:59:50 +0300

    admin sayfası yapıldı

--- NesneLabDeneme1/NesneLabDeneme1/Form2.Designer.cs
+++ NesneLabDeneme1/NesneLabDeneme1/Form2.Designer.cs
@@ -41,28 +41,28 @@
             this.chkboxYellow = new System.Windows.Forms.CheckBox();
             this.chkboxRed = new System.Windows.Forms.CheckBox();
             this.lblColor = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.admingirisi = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Pristina", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(73, 26);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 35);
+            this.label1.Size = new System.Drawing.Size(113, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Settings";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(76, 80);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-1, 50);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 15);
+            this.label2.Size = new System.Drawing.Size(68, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Difficulties";
             // 
@@ -73,8 +73,8 @@
             "easy",
             "normal",
             "hard"});
-            this.comboBox1.Location = new System.Drawing.Point(75, 97);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Location = new System.Drawing.Point(3, 68);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(92, 21);
             this.comboBox1.TabIndex = 2;
@@ -83,11 +83,11 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(83, 213);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(0, 100);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 15);
+            this.label3.Size = new System.Drawing.Size(48, 16);
             this.label3.TabIndex = 3;
             this.label3.Text = "Shape";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -95,8 +95,8 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(75, 230);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox1.Location = new System.Drawing.Point(3, 126);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(64, 17);
             this.checkBox1.TabIndex = 4;
@@ -106,8 +106,8 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(75, 251);
-            this.checkBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox2.Location = new System.Drawing.Point(3, 147);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(60, 17);
             this.checkBox2.TabIndex = 5;
@@ -117,8 +117,8 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(75, 272);
-            this.checkBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox3.Location = new System.Drawing.Point(3, 168);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox3.Name = "checkBox3";
             this.checkBox3.Size = new System.Drawing.Size(52, 17);
             this.checkBox3.TabIndex = 6;
@@ -127,31 +127,31 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(73, 326);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(1, 222);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.Size = new System.Drawing.Size(59, 19);
             this.button1.TabIndex = 7;
-            this.button1.Text = "save";
+            this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(133, 326);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(61, 222);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(56, 19);
+            this.button2.Size = new System.Drawing.Size(58, 19);
             this.button2.TabIndex = 8;
-            this.button2.Text = "quit";
+            this.button2.Text = "Quit";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // chkboxGreen
             // 
             this.chkboxGreen.AutoSize = true;
-            this.chkboxGreen.Location = new System.Drawing.Point(139, 272);
-            this.chkboxGreen.Margin = new System.Windows.Forms.Padding(2);
+            this.chkboxGreen.Location = new System.Drawing.Point(67, 168);
+            this.chkboxGreen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chkboxGreen.Name = "chkboxGreen";
             this.chkboxGreen.Size = new System.Drawing.Size(55, 17);
             this.chkboxGreen.TabIndex = 12;
@@ -161,8 +161,8 @@
             // chkboxYellow
             // 
             this.chkboxYellow.AutoSize = true;
-            this.chkboxYellow.Location = new System.Drawing.Point(139, 251);
-            this.chkboxYellow.Margin = new System.Windows.Forms.Padding(2);
+            this.chkboxYellow.Location = new System.Drawing.Point(67, 147);
+            this.chkboxYellow.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chkboxYellow.Name = "chkboxYellow";
             this.chkboxYellow.Size = new System.Drawing.Size(57, 17);
             this.chkboxYellow.TabIndex = 11;
@@ -172,8 +172,8 @@
             // chkboxRed
             // 
             this.chkboxRed.AutoSize = true;
-            this.chkboxRed.Location = new System.Drawing.Point(139, 230);
-            this.chkboxRed.Margin = new System.Windows.Forms.Padding(2);
+            this.chkboxRed.Location = new System.Drawing.Point(67, 126);
+            this.chkboxRed.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chkboxRed.Name = "chkboxRed";
             this.chkboxRed.Size = new System.Drawing.Size(46, 17);
             this.chkboxRed.TabIndex = 10;
@@ -183,31 +183,31 @@
             // lblColor
             // 
             this.lblColor.AutoSize = true;
-            this.lblColor.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblColor.Location = new System.Drawing.Point(147, 213);
+            this.lblColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColor.Location = new System.Drawing.Point(68, 100);
             this.lblColor.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblColor.Name = "lblColor";
-            this.lblColor.Size = new System.Drawing.Size(54, 15);
+            this.lblColor.Size = new System.Drawing.Size(40, 16);
             this.lblColor.TabIndex = 9;
             this.lblColor.Text = "Color";
             // 
-            // radioButton1
+            // admingirisi
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(75, 147);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
-            this.radioButton1.TabIndex = 13;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.admingirisi.Location = new System.Drawing.Point(2, 246);
+            this.admingirisi.Name = "admingirisi";
+            this.admingirisi.Size = new System.Drawing.Size(117, 25);
+            this.admingirisi.TabIndex = 15;
+            this.admingirisi.Text = "Administrator page";
+            this.admingirisi.UseVisualStyleBackColor = true;
+            this.admingirisi.Visible = false;
+            this.admingirisi.Click += new System.EventHandler(this.admingirisi_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.admingirisi);
             this.Controls.Add(this.chkboxGreen);
             this.Controls.Add(this.chkboxYellow);
             this.Controls.Add(this.chkboxRed);
@@ -221,7 +221,7 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
             this.Text = "Form2";
             this.TopMost = true;
@@ -246,6 +246,6 @@
         private System.Windows.Forms.CheckBox chkboxYellow;
         private System.Windows.Forms.CheckBox chkboxRed;
         private System.Windows.Forms.Label lblColor;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button admingirisi;
     }
 }
