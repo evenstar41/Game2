7df708949e0d9294ac65c2ac41447761512672ce
Berkekacar <kacarberke@icloud.com>
2022-05-09 09:04:35 +0300

    tuş ekleme yer değişme ve profillerin settings kısmı xml profil sayfasına eklendi

--- NesneLabDeneme1/NesneLabDeneme1/Form2.cs
+++ NesneLabDeneme1/NesneLabDeneme1/Form2.cs
@@ -19,57 +19,112 @@ namespace NesneLabDeneme1
         {
             InitializeComponent();
         }
-
+        public int sayı;
         private void button1_Click(object sender, EventArgs e)
         {
-            FileStream fw;
-            StreamWriter sw;
-            fw = new FileStream("nesnelabbb.txt", FileMode.OpenOrCreate, FileAccess.Write);
-
-            sw = new StreamWriter(fw);
-
-            sw.WriteLine(comboBox1.Text);
-
-
-            if (checkBox1.Checked)
+            if (comboBox1.Text == "hard")
             {
-                sw.WriteLine(checkBox1.Text);
-
+                sayı = 15;
             }
-            if (checkBox2.Checked)
+            if (comboBox1.Text == "medium")
             {
-                sw.WriteLine(checkBox2.Text);
-
+                sayı = 9;
             }
-            if (checkBox3.Checked)
+            if (comboBox1.Text == "easy")
             {
-                sw.WriteLine(checkBox3.Text);
-
-            }
-            if (chkboxRed.Checked)
-            {
-                sw.WriteLine(chkboxRed.Text);
-
-            }
-            if (chkboxYellow.Checked)
-            {
-                sw.WriteLine(chkboxYellow.Text);
-
-            }
-            if (chkboxGreen.Checked)
-            {
-                sw.WriteLine(chkboxGreen.Text);
-
+                sayı = 5;
             }
 
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load("kayıtlar.xml");
+            foreach(XmlNode node in xmldoc.DocumentElement)
+            {
+                if (node["username"].InnerText == label4.Text)
+                {
+                    node["difficulties"].InnerText = comboBox1.Text;
+                    node["shape"].InnerText = label5.Text;
+                    node["color"].InnerText = label5.Text;
+                    if (checkBox1.Checked)
+                    {
+                        node["shape"].InnerText += "triangle,";
+                    }
+                    if (checkBox2.Checked)
+                    {
+                        node["shape"].InnerText += "square,";
+                    }
+                    if (checkBox3.Checked)
+                    {
+                        node["shape"].InnerText += "circle";
+                    }
+                    if (chkboxRed.Checked)
+                    {
+                        node["color"].InnerText += "red,";
+                    }
+                    if (chkboxGreen.Checked)
+                    {
+                        node["color"].InnerText += "green,";
+                    }
+                    if (chkboxYellow.Checked)
+                    {
+                        node["color"].InnerText += "yellow";
+                    }
+                }
+            }
+            xmldoc.Save("kayıtlar.xml");
+            
+            //FileStream fw;
+            //StreamWriter sw;
+            //fw = new FileStream("nesnelabbb.txt", FileMode.OpenOrCreate, FileAccess.Write);
 
-            sw.Close();
-            fw.Close();
+            //sw = new StreamWriter(fw);
+
+            //sw.WriteLine(comboBox1.Text);
+
+
+            //if (checkBox1.Checked)
+            //{
+            //    sw.WriteLine(checkBox1.Text);
+
+            //}
+            //if (checkBox2.Checked)
+            //{
+            //    sw.WriteLine(checkBox2.Text);
+
+            //}
+            //if (checkBox3.Checked)
+            //{
+            //    sw.WriteLine(checkBox3.Text);
+
+            //}
+            //if (chkboxRed.Checked)
+            //{
+            //    sw.WriteLine(chkboxRed.Text);
+
+            //}
+            //if (chkboxYellow.Checked)
+            //{
+            //    sw.WriteLine(chkboxYellow.Text);
+
+            //}
+            //if (chkboxGreen.Checked)
+            //{
+            //    sw.WriteLine(chkboxGreen.Text);
+
+            //}
+
+
+            //sw.Close();
+            //fw.Close();
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
             //admin sayfası girişi izni
+
+            label4.Text = Form1.giden;
+            label4.Visible = false;
+
+
             FileStream fs = new FileStream("username.txt", FileMode.Open, FileAccess.Read);
             StreamReader sw = new StreamReader(fs);
             string yazi = sw.ReadLine();
@@ -78,52 +133,98 @@ namespace NesneLabDeneme1
             fs.Close();
             try
             {
-
+ 
 
                 if (yazi == "admin")
                 {
                     admingirisi.Visible = true;
                 }
+                XmlDocument xmldoc = new XmlDocument();
+                xmldoc.Load("kayıtlar.xml");
 
-                StreamReader sr = File.OpenText("nesnelabbb.txt");
-                string metin;
-
-
-                while ((metin = sr.ReadLine()) != null)
+                foreach(XmlNode node in xmldoc.DocumentElement)
                 {
-                    comboBox1.Text = metin;
-                    break;
-                }
-                string metin2;
+                    if (node["username"].InnerText == label4.Text)
+                    {
+                        comboBox1.Text = node["difficulties"].InnerText;
 
-                while ((metin2 = sr.ReadLine()) != null)
-                {
-                    if (metin2 == "Circle")
-                    {
-                        checkBox1.Checked = true;
+                        string[] shape = node["shape"].InnerText.Split(',');
+                        foreach (string index in shape)
+                        {
+                            if (index == "triangle")
+                            {
+                                checkBox1.Checked = true;
+                            }
+                            if (index == "square")
+                            {
+                                checkBox2.Checked = true;
+                            }
+                            if (index == "circle")
+                            {
+                                checkBox3.Checked = true;
+                            }
+
+                        }
+                        string[] color = node["color"].InnerText.Split(',');
+                        foreach(String renk in color)
+                        {
+                            if (renk == "red")
+                            {
+                                chkboxRed.Checked = true;
+                            }
+                            if (renk == "yellow")
+                            {
+                                chkboxYellow.Checked = true;
+                            }
+                            if (renk == "green")
+                            {
+                                chkboxGreen.Checked = true;
+                            }
+                        }
                     }
-                    if (metin2 == "Triangle")
-                    {
-                        checkBox3.Checked = true;
-                    }
-                    if (metin2 == "Square")
-                    {
-                        checkBox2.Checked = true;
-                    }
-                    if (metin2 == "Red")
-                    {
-                        chkboxRed.Checked = true;
-                    }
-                    if (metin2 == "Yellow")
-                    {
-                        chkboxYellow.Checked = true;
-                    }
-                    if (metin2 == "Green")
-                    {
-                        chkboxGreen.Checked = true;
-                    }
+                    
+
+                    
                 }
-                sr.Close();
+                //StreamReader sr = File.OpenText("nesnelabbb.txt");
+                //string metin;
+
+
+                //while ((metin = sr.ReadLine()) != null)
+                //{
+                //    comboBox1.Text = metin;
+                //    break;
+                //}
+                //string metin2;
+
+                //while ((metin2 = sr.ReadLine()) != null)
+                //{
+                //    if (metin2 == "Circle")
+                //    {
+                //        checkBox1.Checked = true;
+                //    }
+                //    if (metin2 == "Triangle")
+                //    {
+                //        checkBox3.Checked = true;
+                //    }
+                //    if (metin2 == "Square")
+                //    {
+                //        checkBox2.Checked = true;
+                //    }
+                //    if (metin2 == "Red")
+                //    {
+                //        chkboxRed.Checked = true;
+                //    }
+                //    if (metin2 == "Yellow")
+                //    {
+                //        chkboxYellow.Checked = true;
+                //    }
+                //    if (metin2 == "Green")
+                //    {
+                //        chkboxGreen.Checked = true;
+                //    }
+                //}
+                //sr.Close();
             }
             catch (Exception)
             {
@@ -157,6 +258,13 @@ namespace NesneLabDeneme1
         {
             new Form5().Show();
             this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form6 f6 = new Form6();
+            f6.Show();
+            this.Close();
         }
     }
 }
---
+++ NesneLabDeneme1/NesneLabDeneme1/Form6.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace NesneLabDeneme1
+{
+    public partial class Form6 : Form
+    {
+        private bool clickTemp = false;
+        private Color color;
+        private string shape;
+        private Button buttontemp;
+        private Button[,] buttonarraytemp;
+        
+        public Form6()
+        {
+            InitializeComponent();
+        }
+        private void random_start()
+        {
+
+        }
+        private void ucekle(Button[,] buton)
+        {
+            List<string> sekil=new List<string> { "▲", "○", "▀" };
+            for (int i = 0; i < 3; i++)
+            {
+                int sayı1;
+                int sayı2;
+                Random random = new Random();
+                do
+                {
+                    
+                    sayı1 = random.Next(8);
+                    sayı2 = random.Next(8);
+                } while (buton[sayı1,sayı2].Text !="");
+                int sayı3 = random.Next(3 - i);
+                if (i == 0)
+                {
+                    buton[sayı1, sayı2].BackColor = Color.Red;
+                    buton[sayı1, sayı2].Text = sekil[sayı3];
+                    sekil.RemoveAt(sayı3);
+                }
+                if (i == 1)
+                {
+                    buton[sayı1, sayı2].BackColor = Color.Green;
+                    buton[sayı1, sayı2].Text = sekil[sayı3];
+                    sekil.RemoveAt(sayı3);
+                }
+                if (i == 2)
+                {
+                    buton[sayı1, sayı2].BackColor = Color.Yellow;
+                    buton[sayı1, sayı2].Text = sekil[sayı3];
+                }
+            }
+        }
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            
+            if (clickTemp == false &&btn.BackColor !=Color.AliceBlue)
+            {
+                color = btn.BackColor;
+                shape = btn.Text;
+                buttontemp = btn;
+                
+                
+                clickTemp = true;
+
+            }
+            if (clickTemp == true && btn.BackColor == Color.AliceBlue)
+            {
+                btn.BackColor = color;
+                btn.Text = shape;
+                buttontemp.BackColor = Color.AliceBlue;
+                buttontemp.Text = "";
+                clickTemp = false;
+                ucekle(buttonarraytemp);
+
+                
+            }
+
+
+            
+            
+
+            
+        }
+        
+
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            ////////////////////tablo yapımı 
+            //Form2 f2 = new Form2();
+            //int sayı = f2.sayı;
+            //Button[,] button0 = new Button[8, 8];
+
+
+            //int top = 0;
+            //int left = 0;
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    for (int j = 0; j < 8; j++)
+            //    {
+
+            //        Button button = new Button();
+            //        button.Size = new Size(60, 60);
+            //        button.Location = new Point(60 * i, 60 * j);
+            //        button.Click += Button_Click;
+            //        panel1.Controls.Add(button);
+            //    }
+            //}
+            int horizotal = 0;
+            int vertical = 40;
+            Button[,] buttonArray = new Button[8,8];
+            buttonarraytemp = buttonArray;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    buttonArray[i,y] = new Button();
+                    buttonArray[i,y].Size = new Size(40, 40);
+                    buttonArray[i,y].Location = new Point(horizotal, vertical);
+                    buttonArray[i,y].BackColor = Color.AliceBlue;
+                    buttonArray[i, y].Click += Button_Click; // click event yükle
+
+                    
+                    horizotal += 40;
+                    this.Controls.Add(buttonArray[i,y]);
+                }
+                vertical += 40;
+                horizotal = 0;
+            }
+            /////////şekil renk ekleme
+
+            List<string> sekiller = new List<string> { "▲", "○", "▀" };
+
+
+
+            /////////////
+
+            Random random_yellow = new Random();
+            int rand_yellow = random_yellow.Next(8);
+
+            
+            int rand_yellowy = random_yellow.Next(8);
+
+            int rand_yellow_s = random_yellow.Next(3);
+
+            buttonArray[rand_yellow, rand_yellowy].BackColor = Color.Yellow;
+            buttonArray[rand_yellow, rand_yellowy].Text = sekiller[rand_yellow_s];
+
+            sekiller.RemoveAt(rand_yellow_s);
+            ////////
+            int rand_green = random_yellow.Next(8);
+
+            int rand_greeny = random_yellow.Next(8);
+
+            int rand_green_s = random_yellow.Next(2);
+
+            buttonArray[rand_green, rand_greeny].BackColor = Color.Green;
+            buttonArray[rand_green, rand_greeny].Text = sekiller[rand_green_s];
+
+            /////
+            int rand_red = random_yellow.Next(8);
+
+            int rand_redy = random_yellow.Next(8);
+
+            int rand_red_s = random_yellow.Next(1);
+
+            buttonArray[rand_red, rand_redy].BackColor = Color.Red;
+            buttonArray[rand_red, rand_redy].Text = sekiller[rand_red_s];
+
+
+
+
+
+
+        }
+    }
+}
